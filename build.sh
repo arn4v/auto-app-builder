e051ca367b8134396b27a91236125bb524aacd4d
Arnav Gosain <arn4v@tutanota.com>
2020-05-14 22:15:21 +0530

    lazy_builder: version 1
    Lazy Builder is a script for automating the build process for FOSS apps for privacy-focused users that want to be able to build and sign apps they use.
    
    The current version can do the following:
    * Read json file for listed apps, the repositories they are in and the latest tag. When running for the first time, the script will write what tag of what app it is building to .tags.ini which it
      it will read for subsequent runs to see if an app has already been built and skip if it has been on the latest tag. This implementation needs to be changed in order to drop reliance on the date of build
      to skip apps that have already been built.
    
    To-Do:
    * Add a "build <app>" and buildall functions.
    * Discard usage of .tags.ini and shift to writing tags in the required apps.json
    * Add ability to determine which apps sign as a part of the gradle wrapper build process and add ability to sign apps that don't do that in the gradle wrapper.
    * Add the ability to add apps to the json or create an entirely new json using an "addapp <app-name> <repository> <branch>" usage function.
    
    Signed-off-by: Arnav Gosain <arn4v@tutanota.com>

---
+++ build.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+# Copyright (C) 2020 Arnav Gosain
+# SPDX-License-Identifier: GNU General Public License v3.0 only
+
+# Uncomment this with your own keystore properties file
+# . ~/.keystores/props-myapps
+# Properties file template:
+# keystoreFile=
+# keystorePassword=
+# keystoreAlias=
+# keyPassword=
+
+try() {
+    [[ $- = *e* ]]; SAVED_OPT_E=$?
+    set +e
+}
+
+catch() {
+    export ex_code=$?
+    (( $SAVED_OPT_E )) && set +e
+    return $ex_code
+}
+
+if [[ ! $1 == "SmartPack" ]]; then
+    sed -i 's/org.gradle.jvmargs=.*//' gradle.properties
+    echo "org.gradle.jvmargs=-Xmx3g -XX:MaxPermSize=2048m -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8" >> gradle.properties
+else
+    echo "Nothing to do here"
+fi
+
+if [[ $1 == "LibreraPro" ]] ;then
+    touch ~/.gradle/gradle.properties
+    echo "RELEASE_STORE_FILE=~/.keystores/myapps.pkcs12" >> ~/.gradle/gradle.properties
+    echo "RELEASE_STORE_PASSWORD=$keystorePassword" >> ~/.gradle/gradle.properties
+    echo "RELEASE_KEY_ALIAS=$keystoreAlias" >> ~/.gradle/gradle.properties
+    echo "RELEASE_KEY_PASSWORD=$keystorePassword" >> ~/.gradle/gradle.properties
+    bash Builder/link_to_mupdf_1.16.1.sh
+    ./gradlew clean assembleFdroid || echo "Failed"
+    rm ~/.gradle/gradle.properties
+elif [[ $1 == "AntennaPod" ]]; then
+    echo "releaseStoreFile=$keystoreFile" >> gradle.properties
+    echo "releaseStorePassword=$keystorePassword" >> gradle.properties
+    echo "releaseKeyAlias=$keystoreAlias" >> gradle.properties
+    echo "releaseKeyPassword=$keystorePassword" >> gradle.properties
+    ./gradlew clean build || echo "Failed"
+elif [[ $1 == "SmartPack" ]]; then
+    ./gradlew clean build || echo "Failed"
+else
+    ./gradlew clean build || echo "Failed"
+fi
---
+++ start.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+# Copyright (C) 2020 Arnav Gosain
+# SPDX-License-Identifier: GNU General Public License v3.0 only
+
+root_dir=$(pwd)
+working_dir=$root_dir/apps
+out_dir=$root_dir/out
+app_list=$root_dir/apps.json
+
+get_release_info() {
+    curl --silent "https://api.github.com/repos/$1/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'
+}
+
+try() {
+    [[ $- = *e* ]]; SAVED_OPT_E=$?
+    set +e
+}
+
+catch() {
+    export ex_code=$?
+    (( $SAVED_OPT_E )) && set +e
+    return $ex_code
+}
+
+echo "Root Directory: $root_dir"
+echo "Working Directory: $working_dir"
+printf "Building:\n$(cat $app_list | jq -r .[].app)"
+echo " "
+
+for app in $(cat $app_list | jq -r .[].app); do
+    try
+    apprepo=$(cat $app_list | jq -r .[].$app[]?.repository | head -n 1)
+    appbranch=$(cat $app_list | jq -r .[].$app[]?.branch | head -n 1)
+    tag=$(get_release_info $apprepo)
+    if [[ ! -d out ]]; then mkdir out; fi
+    if [[ ! -f .tags.ini ]]; then touch .tags.ini; fi
+    if [[ $(cat .tags.ini | grep $app | sed "s/${app}.*/${app}/") == $app ]] && [[ $(cat .tags.ini | grep $app | sed "s/${app}://") == $tag ]] && [[ -d "$out_dir/$app-$tag-$(date +%Y%m%d)" ]] && [[ $(ls "$out_dir/$app-$tag-$(date +%Y%m%d)" | head -c1 | wc -c) -ne 0 ]]; then
+        echo "Already build $app with release tag $tag sources, continuing."
+        continue
+    else
+	if [[ $(cat .tags.ini | grep $app | head -c1 | wc -c) -ne 0 ]]; then sed -i "s/${app}.*//" .tags.ini; fi
+	echo "$app:$tag" >> .tags.ini
+	rm -rf $working_dir
+	mkdir $working_dir && cd $working_dir
+	echo "Downloading $app source"
+	echo " "
+	wget https://github.com/$apprepo/archive/$tag.zip
+	echo "Decompressing $app source"
+	echo " "
+	unzip *.zip
+	rm *zip
+	cd $working_dir/*
+	echo "sdk.dir=${ANDROID_SDK_PATH}" >> "local.properties"
+	echo "Building $app with tag: $tag sources"
+	echo " "
+	bash $root_dir/build.sh $app
+        if [[ ! -d "$out_dir/$app-$tag-$(date +%Y%m%d)" ]]; then mkdir $out_dir/$app-$tag-$(date +%Y%m%d); fi
+	find $working_dir/ -iname '*unsigned*.apk' -exec mv {} $out_dir/$app-$tag-$(date +%Y%m%d) \;
+        ls $out_dir/$app-$tag-$(date +%Y%m%d)/ | xargs -I {} mv {} $app-$tag-$(date +%Y%m%d_%H%M)_{}
+        killall -9 java && pkill -f '.*GradleDaemon.*'
+	cd $root_dir
+    fi
+done
