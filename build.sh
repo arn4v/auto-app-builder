9ec76b79050440fa547f53a1a34d00e93e93f45a
Arnav Gosain <arn4v@tutanota.com>
2020-05-16 11:53:42 +0530

    fix it for good

--- build.sh
+++ build.sh
@@ -13,41 +13,30 @@ catch() {
     return $ex_code
 }
 
-if [[ $1 == "SmartPack" ]]; then
-else
+if [[ ! $1 == "SmartPack" ]]; then
     sed -i 's/org.gradle.jvmargs=.*//' gradle.properties
     echo "org.gradle.jvmargs=-Xmx3g -XX:MaxPermSize=2048m -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8" >> gradle.properties
+else
+    echo "Nothing to do here"
 fi
 
 if [[ $1 == "LibreraPro" ]] ;then
-    try
     touch ~/.gradle/gradle.properties
     echo "RELEASE_STORE_FILE=~/.keystores/myapps.pkcs12" >> ~/.gradle/gradle.properties
     echo "RELEASE_STORE_PASSWORD=$keystorePassword" >> ~/.gradle/gradle.properties
     echo "RELEASE_KEY_ALIAS=$keystoreAlias" >> ~/.gradle/gradle.properties
     echo "RELEASE_KEY_PASSWORD=$keystorePassword" >> ~/.gradle/gradle.properties
     bash Builder/link_to_mupdf_1.16.1.sh
-    catch || {
     ./gradlew clean assembleFdroid || echo "Failed"
-    }
     rm ~/.gradle/gradle.properties
 elif [[ $1 == "AntennaPod" ]]; then
-    try
     echo "releaseStoreFile=$keystoreFile" >> gradle.properties
     echo "releaseStorePassword=$keystorePassword" >> gradle.properties
     echo "releaseKeyAlias=$keystoreAlias" >> gradle.properties
     echo "releaseKeyPassword=$keystorePassword" >> gradle.properties
-    catch || {
     ./gradlew clean build || echo "Failed"
-    }
 elif [[ $1 == "SmartPack" ]]; then
-    try
-    catch || {
     ./gradlew clean build || echo "Failed"
-    }
 else
-    try
-    catch || {
     ./gradlew clean build || echo "Failed"
-    }
 fi
--- start.sh
+++ start.sh
@@ -29,33 +29,28 @@ for app in $(cat $app_list | jq -r .[].app); do
     appbranch=$(cat $app_list | jq -r .[].$app[]?.branch | head -n 1)
     tag=$(get_release_info $apprepo)
     if [[ ! -d out ]]; then mkdir out; fi
-    if [[ ! -f .tags.ini ]]; then
-	touch .tags.ini
-    fi
-    if [[ ! $(cat .tags.ini | grep $app | sed "s/$app:.*/$app/") == $app ]]; then
-      if [[ $(cat tags.txt | grep $app | sed 's/$app://') == $tag ]]; then
-        if [[ ! -n $(ls $root_dir/out/$app-$tag-*.apk) ]]; then
-            echo "${app}:${tag}" >> .tags.ini
-            rm -rf $working_dir
-            mkdir $working_dir && cd $working_dir
-            echo "Downloading $app source"
-            echo " "
-            wget https://github.com/$apprepo/archive/$tag.zip
-            echo "Decompressing $app source"
-            echo " "
-            unzip *.zip
-            rm *zip
-            cd $working_dir/*
-            echo "sdk.dir=${ANDROID_SDK_PATH}" >> "local.properties"
-            echo "Building $app with tag: $tag sources"
-            echo " "
-            bash $root_dir/build.sh $app
-            for appfile in $(find $working_dir/ -type f -name '*unsigned*apk'); do mv -- $appfile $root_dir/out/"$app-$tag-$(date +%Y%m%d_%H%M)-$appfile"; done
-            killall -9 java && pkill -f '.*GradleDaemon.*'
-        else
-            echo "Already build $app with release tag $tag sources, continuing."
-            continue
-        fi
-      fi
+    if [[ ! -f .tags.ini ]]; then touch .tags.ini; fi
+    if [[ $(cat .tags.ini | grep $app | sed "s/${app}.*/${app}/") == $app ]] && [[ $(cat .tags.ini | grep $app | sed "s/${app}://") == $tag ]] && [[ $(test -n "$(find $root_dir/out -type f -name $app-$tag-*.apk)" | head -c1 | wc -c) -ne 0 ]]; then
+        echo "Already build $app with release tag $tag sources, continuing."
+        continue
+    else
+	sed -i "s/${app}.*//" .tags.ini
+	echo "$app:$tag" >> .tags.ini
+	rm -rf $working_dir
+	mkdir $working_dir && cd $working_dir
+	echo "Downloading $app source"
+	echo " "
+	wget https://github.com/$apprepo/archive/$tag.zip
+	echo "Decompressing $app source"
+	echo " "
+	unzip *.zip
+	rm *zip
+	cd $working_dir/*
+	echo "sdk.dir=${ANDROID_SDK_PATH}" >> "local.properties"
+	echo "Building $app with tag: $tag sources"
+	echo " "
+	bash $root_dir/build.sh $app
+	for appfile in $(find $working_dir/ -type f -name '*unsigned*apk'); do mv -- $appfile $root_dir/out/"$app-$tag-$(date +%Y%m%d_%H%M)-$appfile"; done
+	killall -9 java && pkill -f '.*GradleDaemon.*'
     fi
 done
